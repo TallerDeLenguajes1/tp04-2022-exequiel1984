77f1b0d666d521d7ba99c9d2b153f26d0a63095f
Carrizo Miranda Hector Exequiel <exequiel1984carrizo@gmail.com>
2022-04-28 11:31:58 -0300

    Solo llegue a cargar la lista de tareas realizadas y las muestro

--- ToDo.c
+++ ToDo.c
@@ -3,130 +3,184 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct Tarea { 
+struct Trabajo{ 
     int TareaID;    //Numerado en ciclo iterativo 
     char *Descripcion;  // 
     int Duracion; // entre 10 – 100 
-};
+}typedef Tarea;
 
-void buscarTarea(struct Tarea **, struct Tarea **, int, char *);
+struct TNodo{
+    Tarea T;
+    struct TNodo *siguiente;
+} typedef Nodo;
+
+//********************DECLARO FUNCIONES***************
+
+void CrearListaVacia(Nodo **);
+void MostrarLista(Nodo *);
+void CargarListaAlFinal(Nodo **, int, char *);
+void LiberarLista(Nodo *);
+
+//*********************MAIN***************************
 
 int main(){
-    int cantidadTareas,j;
-    struct Tarea **TareasPendientes;
-    struct Tarea **TareasRealizadas;
+
+    int CantidadTareasACargar;
     char *Buff;
     int ControlTarea; //1=Si, 0=No
     int ContadorTareasRealizadas = 0;
     char *PalabraClave;
 
-    srand(time(NULL));
-
     Buff = (char *) malloc(100 * sizeof(char));
 
+    srand(time(NULL));
+    Nodo *TareasPendientes, *TareasRealizadas;
+    
+    CrearListaVacia(&TareasPendientes);
+    CrearListaVacia(&TareasRealizadas);
+
     printf("\nIngrese la cantidad de tareas a cargar: ");
-    scanf("%d", &cantidadTareas);
+    scanf("%d", &CantidadTareasACargar);
     getchar();
-//Reservo memoria de tareas pendientes
-    TareasPendientes = (struct Tarea **) malloc(cantidadTareas * sizeof(struct Tarea *));
-    TareasRealizadas = (struct Tarea **) malloc(cantidadTareas * sizeof(struct Tarea*));
 
-    for (int i = 0; i < cantidadTareas; i++)
+    for (int i = 0; i < CantidadTareasACargar; i++)
     {
-        TareasPendientes[i] = (struct Tarea *) malloc(sizeof(struct Tarea));
+        CargarListaAlFinal(&TareasPendientes, i, Buff);
     }
     
-//Cargo datos
 
-    for (int i = 0; i < cantidadTareas; i++)
-    {
-        TareasPendientes[i]->TareaID = i+1;
-        printf("\nTarea %d", TareasPendientes[i]->TareaID);
-        printf("\nDescripcion: ");
-        gets(Buff);
+    printf("\n*****LISTA DE TAREAS PENDIENTES*****\n");
+    MostrarLista(TareasPendientes);
 
-        TareasPendientes[i]->Descripcion = (char *) malloc(strlen(Buff)+1 * sizeof(char));
-        strcpy(TareasPendientes[i]->Descripcion, Buff);
 
-        TareasPendientes[i]->Duracion = 10 + rand() % 100 - 10;
-
-        printf("Duracion: %d\n", TareasPendientes[i]->Duracion);
-    }
-
-//Muestro tareas cargadas
-    printf("\n\n*****CONTROL DE TAREAS PENDIENTES CARGADAS*****\n");
-
-    for (int i = 0; i < cantidadTareas; i++)
-    {
-        printf("\nTarea %d", TareasPendientes[i]->TareaID);
-        printf("\nDescripcion: %s", TareasPendientes[i]->Descripcion);
-        printf("\nDuracion: %d", TareasPendientes[i]->Duracion);
-
-        printf("\n¿La tarea se realizo (Si = 1, No = 0? ");
-        scanf("%d", &ControlTarea);
-
-        if (ControlTarea)
-        {
-            TareasRealizadas[ContadorTareasRealizadas] = TareasPendientes[i];
-            TareasPendientes[i] = NULL;
-            ContadorTareasRealizadas++;
-        } 
-    }
-
-    printf("\n*****LISTA DE TAREAS REALIZADAS*****\n");
-
-    for (int i = 0; i < ContadorTareasRealizadas; i++)
-    {
-        printf("\nTarea: %d", TareasRealizadas[i]->TareaID);
-        printf("\nDescripcion: %s", TareasRealizadas[i]->Descripcion);
-        printf("\nDuracion: %d\n", TareasRealizadas[i]->Duracion);
-    }
-
-    printf("\n\n*****TAREAS PENDIENTES*****\n");
-
-      for (int i = 0; i < cantidadTareas; i++)
-    {
-        if (TareasPendientes[i] != NULL)
-        {
-            printf("\nTarea: %d", TareasPendientes[i]->TareaID);
-            printf("\nDescripcion: %s", TareasPendientes[i]->Descripcion);
-            printf("\nDuracion: %d\n", TareasPendientes[i]->Duracion);
-        }
-    }
     
-    puts("\n*****BUSCADOR DE TAREA*****");
 
-    printf("\nIngrese la palabra clave: ");
-    gets(Buff);
 
-    PalabraClave = (char *) malloc(strlen(Buff)+1 * sizeof(char));
-    strcpy(PalabraClave, Buff);
+    // printf("\n\n*****CONTROL DE TAREAS PENDIENTES CARGADAS*****\n");
 
-    for (int i = 0; i < cantidadTareas; i++)
-    {
-        if (!strcmp(PalabraClave, TareasPendientes[i]->Descripcion))
-        {
-            printf("Tarea Pendiente");
-            printf("\nTarea: %d", TareasPendientes[i]->TareaID);
-            printf("\nDescripcion: %s", TareasPendientes[i]->Descripcion);
-            printf("\nDuracion: %d\n", TareasPendientes[i]->Duracion);
-        } 
-    }
+        
+
+    //     printf("\n¿La tarea se realizo (Si = 1, No = 0? ");
+    //     scanf("%d", &ControlTarea);
+
+    //     if (ControlTarea)
+    //     {
+    //         TareasRealizadas[ContadorTareasRealizadas] = TareasPendientes[i];
+    //         TareasPendientes[i] = NULL;
+    //         ContadorTareasRealizadas++;
+    //     } 
+    // }
+
+    // printf("\n*****LISTA DE TAREAS REALIZADAS*****\n");
+
+    // for (int i = 0; i < ContadorTareasRealizadas; i++)
+    // {
+    //     printf("\nTarea: %d", TareasRealizadas[i]->TareaID);
+    //     printf("\nDescripcion: %s", TareasRealizadas[i]->Descripcion);
+    //     printf("\nDuracion: %d\n", TareasRealizadas[i]->Duracion);
+    // }
+
+    // printf("\n\n*****TAREAS PENDIENTES*****\n");
+
+    //   for (int i = 0; i < cantidadTareas; i++)
+    // {
+    //     if (TareasPendientes[i] != NULL)
+    //     {
+    //         printf("\nTarea: %d", TareasPendientes[i]->TareaID);
+    //         printf("\nDescripcion: %s", TareasPendientes[i]->Descripcion);
+    //         printf("\nDuracion: %d\n", TareasPendientes[i]->Duracion);
+    //     }
+    // }
+    
+    // puts("\n*****BUSCADOR DE TAREA*****");
+
+    // printf("\nIngrese la palabra clave: ");
+    // gets(Buff);
+
+    // PalabraClave = (char *) malloc(strlen(Buff)+1 * sizeof(char));
+    // strcpy(PalabraClave, Buff);
+
+    // for (int i = 0; i < cantidadTareas; i++)
+    // {
+    //     if (!strcmp(PalabraClave, TareasPendientes[i]->Descripcion))
+    //     {
+    //         printf("Tarea Pendiente");
+    //         printf("\nTarea: %d", TareasPendientes[i]->TareaID);
+    //         printf("\nDescripcion: %s", TareasPendientes[i]->Descripcion);
+    //         printf("\nDuracion: %d\n", TareasPendientes[i]->Duracion);
+    //     } 
+    // }
 
     //Libero memorias
     free(Buff);
 
-    for (int i = 0; i < cantidadTareas; i++)
-    {
-        free(TareasPendientes[i]);
-    }
+    LiberarLista(TareasPendientes);
     
-    free(TareasPendientes);
     
-    free(TareasRealizadas);
+    // free(TareasRealizadas);
 
-    free(PalabraClave);
+    // free(PalabraClave);
 
     getchar();
     return 0;
+}
+
+//**************************FUNCIONES******************
+
+void CrearListaVacia(Nodo **Lista){
+    *Lista=NULL;
+}
+
+
+void CargarListaAlFinal(Nodo **NuevoNodo, int i, char *Buff){
+    Nodo *NodoAux;
+    Nodo *Aux2 = *NuevoNodo;
+    NodoAux = (Nodo *) malloc(sizeof(Nodo));
+
+    NodoAux->T.TareaID = i+1;
+    printf("\nTarea %d", NodoAux->T.TareaID);
+    printf("\nDescripcion: ");
+    gets(Buff);
+    NodoAux->T.Descripcion = (char *) malloc(strlen(Buff)+1 * sizeof(char));
+    strcpy(NodoAux->T.Descripcion, Buff);
+    NodoAux->T.Duracion = 10 + rand() % 100 - 10;
+    printf("Duracion: %d\n", NodoAux->T.Duracion);
+
+    NodoAux->siguiente = NULL;
+    if (Aux2 != NULL)
+    {
+        while (Aux2->siguiente != NULL)
+        {
+            Aux2 = Aux2->siguiente;
+        }
+        Aux2->siguiente = NodoAux;
+    } else{
+        *NuevoNodo = NodoAux;//estoy cargando al final de la fila
+    }
+}
+
+void MostrarLista(Nodo *Lista){
+    int i=0;
+
+    while (Lista!=NULL)
+    {
+        printf("\nTarea %d", Lista->T.TareaID);
+        printf("\nDescripcion: %s", Lista->T.Descripcion);
+        printf("\nDuracion: %d\n", Lista->T.Duracion);
+
+        Lista = Lista->siguiente;
+        i++;
+    }
+}
+
+void LiberarLista(Nodo *Lista){
+    Nodo *Aux;
+    while (Lista != NULL)
+    {
+        Aux = Lista;
+        Lista = Lista->siguiente;
+        free(Aux->T.Descripcion);
+        free(Aux);
+    }
+    
 }
